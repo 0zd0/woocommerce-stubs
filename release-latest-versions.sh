4ec3d5bbdacc52de66f6a226213dee6640979b38
0zd0 <67220210+0zd0@users.noreply.github.com>
2024-12-30 07:01:08 +0000

    Generate stubs for WooCommerce 9.5.1

---
+++ release-latest-versions.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+#
+# Generate WooCommerce stubs from all the latest versions.
+#
+
+set -e
+
+WC_JSON="$(curl -s "https://api.wordpress.org/plugins/info/1.0/woocommerce.json")"
+echo "$WC_JSON" | jq '.versions'
+
+# https://wordpress.org/plugins/woocommerce/advanced/
+for V in 9.5 9.6; do
+    # Find latest version
+    printf -v JQ_FILTER '."versions" | keys[] | select(test("^%s\\\\.%s\\\\.\\\\d+$"))' "${V%.*}" "${V#*.}"
+    LATEST="$(jq -r "$JQ_FILTER" <<<"$WC_JSON" | sort -t "." -k 3 -g | tail -n 1)"
+    if [ -z "$LATEST" ]; then
+        echo "No version for ${V}!"
+        continue
+    fi
+
+    echo "Releasing version ${LATEST} ..."
+
+    if git rev-parse "refs/tags/v${LATEST}" >/dev/null 2>&1; then
+        echo "Tag exists!"
+        continue
+    fi
+
+    # Clean up source/ directory
+    git status --ignored --short -- source/ | sed -n -e 's#^!! ##p' | xargs --no-run-if-empty -- rm -rf
+    # Get new version
+    printf -v SED_EXP 's#\\("woocommerce/woocommerce"\\): "[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+"#\\1: "%s"#' "${LATEST}"
+#    sed -i -e "$SED_EXP" source/composer.json
+    #composer run-script post-install-cmd
+    ## FIXME
+
+    #composer run-script post-install-cmd
+    curl -L "https://downloads.wordpress.org/plugin/woocommerce.${LATEST}.zip" -o "source/woocommerce.${LATEST}.zip"
+    unzip -q -d source/ source/woocommerce.*.zip
+
+    # Generate stubs
+    echo "Generating stubs ..."
+    bash ./generate.sh
+
+    # Tag version
+    git commit --all -m "Generate stubs for WooCommerce ${LATEST}"
+    git tag "v${LATEST}"
+done
